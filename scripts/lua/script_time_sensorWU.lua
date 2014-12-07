ddc3f101b38bd66e89df741e2b96a23d70b6f576
krotof1a <christophe.safra@laposte.net>
2014-12-07 18:02:02 +0100

    Save Domoticz scripts

---
+++ scripts/lua/script_time_sensorWU.lua
@@ -0,0 +1,11 @@
+sensorwu = 'Pression Atmosphérique' --sensor from wunderground
+local tempsensor = 'Temp Extérieur' --virtual temp sensor you created
+local idx = 13 --idx of your virtual temp sensor of wunderground
+commandArray = {}
+time = os.date("*t")
+if((time.min % 10)==0)then
+	sWeatherTemp, sWeatherHumidity, sWeatherPressure = otherdevices_svalues[sensorwu]:match("([^;]+);([^;]+);([^;]+)")
+	sWeatherTemp = tonumber(sWeatherTemp)
+	commandArray['UpdateDevice'] = idx .. '|0|' .. tostring(sWeatherTemp)
+end
+return commandArray
