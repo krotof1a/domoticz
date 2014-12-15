55edfcbed61e90c55cf53ca9f5d8317dee9feeb5
krotof1a <christophe.safra@gmail.com>
2014-12-15 09:04:03 +0100

    Ajout/Correction traces.

--- scripts/lua/script_device_chaudiere.lua
+++ scripts/lua/script_device_chaudiere.lua
@@ -35,8 +35,10 @@
 commandArray = {}
 if (devicechanged['Etat Chaudiere'] == 'On') then
 	os.execute ('/home/pi/tools/radioEmission '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 on')
+	print('(Inter) Chaudiere allumee.')
 end
 if (devicechanged['Etat Chaudiere'] == 'Off') then
 	os.execute ('/home/pi/tools/radioEmission '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 off')
+	print('(Inter) Chaudiere eteinte.')
 end
 return commandArray
