56f8dc8a4bb1807ff5e7aaed07335c7a4e653ef2
krotof1a <christophe.safra@laposte.net>
2014-12-20 23:07:11 +0100

    Add new power loss check script

---
+++ scripts/lua/script_time_alive.lua
@@ -0,0 +1,21 @@
+
+t1 = os.time()
+s = uservariables_lastupdate['StillAlive']
+year = string.sub(s, 1, 4)
+month = string.sub(s, 6, 7)
+day = string.sub(s, 9, 10)
+hour = string.sub(s, 12, 13)
+minutes = string.sub(s, 15, 16)
+seconds = string.sub(s, 18, 19)
+t2 = os.time{year=year, month=month, day=day, hour=hour, min=minutes, sec=seconds}
+difference = (os.difftime (t1, t2))
+
+commandArray = {}
+
+if (difference > 120) then
+	commandArray['SendNotification']='Alerte coupure de courant#Le courant a été coupé à '..s..' et vient de revenir.'
+end 
+
+commandArray['Variable:StillAlive']='1'
+
+return commandArray
