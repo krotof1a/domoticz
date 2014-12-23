b349222bda53a4aeec7270d47c9ab9917ad225f7
krotof1a <christophe.safra@laposte.net>
2014-12-23 14:46:23 +0100

    Replace use of DIO by Casto plug

--- scripts/lua/script_device_chaudiere.lua
+++ scripts/lua/script_device_chaudiere.lua
@@ -1,10 +1,10 @@
 commandArray = {}
 if (devicechanged['Etat Chaudiere'] == 'On') then
-	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 on')
+	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' casto 1381719')
 	print('(Inter) Chaudiere allumee.')
 end
 if (devicechanged['Etat Chaudiere'] == 'Off') then
-	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 off')
+	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' casto 1381716')
 	print('(Inter) Chaudiere eteinte.')
 end
 return commandArray
