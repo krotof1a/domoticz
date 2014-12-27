79d89d6eb08aa4bc1df4c9d5ff4bea1892728889
krotof1a <christophe.safra@laposte.net>
2014-12-27 12:35:07 +0100

    Adapt to reworked Casto handling

--- scripts/lua/script_device_luxSalon.lua
+++ scripts/lua/script_device_luxSalon.lua
@@ -1,10 +1,10 @@
 commandArray = {}
 if (devicechanged['Lumière Salon'] == 'On') then
-	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' casto 1381719')
+	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 on')
 	print('(Inter) Salon allume.')
 end
 if (devicechanged['Lumière Salon'] == 'Off') then
-	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' casto 1381716')
+	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 off')
 	print('(Inter) Salon eteint.')
 end
 return commandArray
