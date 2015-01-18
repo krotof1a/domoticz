8d26642b171c5070b68f233cc29efdc6d9713015
krotof1a <christophe.safra@laposte.net>
2015-01-18 15:51:21 +0100

    Update scripts

---
+++ scripts/lua/script_device_chauffage.lua
@@ -0,0 +1,10 @@
+commandArray = {}
+if (devicechanged['Chauffage Abrit-piscine'] == 'On') then
+	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 on')
+	print('(Inter) Chauffage abrit-piscine allume.')
+end
+if (devicechanged['Chauffage Abrit-piscine'] == 'Off') then
+	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 off')
+	print('(Inter) Chauffage abrit-piscine eteint.')
+end
+return commandArray
