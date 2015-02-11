0e2321eb25edaaa61c794fae839c63d36e588088
krotof1a <christophe.safra@laposte.net>
2015-02-11 20:17:35 +0100

    Move code from Domoticz Event GUI

--- scripts/lua/script_device_luxCouloir.lua
+++ scripts/lua/script_device_luxCouloir.lua
@@ -3,4 +3,9 @@ if (devicechanged['Lumière Couloir'] == 'On') then
 	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 3 pulse 50')
 	print('(Inter) Couloir actionne.')
 end
+if (devicechanged['Porte Entrée'] == 'Open') then
+	commandArray['Lumière Couloir']='On'
+	print('(Porte) Ouverture porte d entree detectee.')
+end
+
 return commandArray
