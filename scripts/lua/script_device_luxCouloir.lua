2d12b2a78eae95a758bf528428dd0ba4f687e0bc
krotof1a <christophe.safra@laposte.net>
2015-02-12 20:13:35 +0100

    Add contraint time

--- scripts/lua/script_device_luxCouloir.lua
+++ scripts/lua/script_device_luxCouloir.lua
@@ -1,11 +1,18 @@
 commandArray = {}
+
+heureMax = 2030
+
 if (devicechanged['Lumière Couloir'] == 'On') then
 	os.execute ('/home/pi/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 3 pulse 50')
 	print('(Inter) Couloir actionne.')
 end
-if (devicechanged['Porte Entrée'] == 'Open') then
-	commandArray['Lumière Couloir']='On'
-	print('(Porte) Ouverture porte d entree detectee.')
+if (devicechanged['Porte Entrée'] == 'Open' and timeofday['Nighttime']) then
+	t1 = os.date()
+	hh = string.sub(t1,12,13)
+	mm = string.sub(t1,15,16)
+	if ((hh*100+mm) < heureMax) then
+		commandArray['Lumière Couloir']='On'
+		print('(Porte) Ouverture porte d entree detectee.')
+	end
 end
-
 return commandArray
