e3dc86c10b18eae196080b6705df79d5218a41c3
krotof1a <christophe.safra@gmail.com>
2015-02-18 11:04:47 +0100

    Merge lux scripts

--- scripts/lua/script_device_luxGarage.lua
+++
@@ -1,6 +0,0 @@
-commandArray = {}
-if (devicechanged['Lumière Garage'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 pulse 50')
-	print('(Inter) Garage actionne.')
-end
-return commandArray
