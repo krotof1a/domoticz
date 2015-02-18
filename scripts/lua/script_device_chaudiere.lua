ebcde4fdecbbc7f04b57737a07444d781e1c2c87
krotof1a <christophe.safra@gmail.com>
2015-02-18 11:17:38 +0100

    Merge chauffage scripts

--- scripts/lua/script_device_chaudiere.lua
+++ scripts/lua/script_device_chaudiere.lua
@@ -7,4 +7,12 @@ if (devicechanged['Etat Chaudiere'] == 'Off') then
 	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 1 off')
 	print('(Inter) Chaudiere eteinte.')
 end
+if (devicechanged['Chauffage Abrit-piscine'] == 'On') then
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 on')
+	print('(Inter) Chauffage abrit-piscine allume.')
+end
+if (devicechanged['Chauffage Abrit-piscine'] == 'Off') then
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 off')
+	print('(Inter) Chauffage abrit-piscine eteint.')
+end
 return commandArray
