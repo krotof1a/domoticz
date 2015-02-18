22324442f9070e9939222a7f296244a4c61470d4
krotof1a <christophe.safra@gmail.com>
2015-02-18 11:15:22 +0100

    Merge portail scripts

--- scripts/lua/script_device_portillon.lua
+++
@@ -1,6 +0,0 @@
-commandArray = {}
-if (devicechanged['Portillon'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' portal '..uservariables['Portail_code']..' 5000')
-	print('(Inter) Portillon actionne.')
-end
-return commandArray
