0b5d29e7928d102ed1f323759b615be16d158bf2
krotof1a <christophe.safra@gmail.com>
2015-03-02 06:49:18 +0100

    Move door opening apart

--- scripts/lua/script_device_lux.lua
+++ scripts/lua/script_device_lux.lua
@@ -5,18 +5,6 @@ if (devicechanged['Lumière Couloir'] == 'On') then
 	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 3 pulse 50')
 	print('(Inter) Couloir actionne.')
 end
-if (devicechanged['Porte Entrée'] == 'Open' and timeofday['Nighttime']) then
-	heureMin = 800
-	heureMax = 2030
-	t1 = os.date()
-	hh = string.sub(t1,12,13)
-	mm = string.sub(t1,15,16)
-	t2 = hh*100+mm
-	if (t2 > heureMin and t2 < heureMax) then
-		commandArray['Lumière Couloir']='On'
-		print('(Porte) Ouverture porte d entree detectee.')
-	end
-end
 
 -- Handle Parking light actions and triggers
 if (devicechanged['Lumière Garage'] == 'On') then
