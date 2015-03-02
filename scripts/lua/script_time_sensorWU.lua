4fe606a327a3dddf46233d89b47e915ce5f24304
krotof1a <christophe.safra@laposte.net>
2015-03-03 00:35:49 +0100

    Change WU sensor name

--- scripts/lua/script_time_sensorWU.lua
+++ scripts/lua/script_time_sensorWU.lua
@@ -1,4 +1,5 @@
 sensorwu = 'Station Meteo' --sensor from wunderground
+local tempsensor = 'Temp Extérieur' --virtual temp sensor you created
 local idx = 13 --idx of your virtual temp sensor of wunderground
 commandArray = {}
 time = os.date("*t")
