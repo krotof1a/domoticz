0dde9e0dba61d7d41415c0996561d763119a9058
krotof1a <christophe.safra@gmail.com>
2015-03-04 09:12:07 +0100

    Use helper script

--- scripts/lua/script_device_security.lua
+++ scripts/lua/script_device_security.lua
@@ -3,10 +3,10 @@ commandArray = {}
 if (devicechanged['Switch Niveau Sécurité'] == 'On') then
 	if (globalvariables['Security'] == 'Disarmed') then
 		print('(Security) Passage de Désarmé à Armé.')
-		os.execute('wget -o /dev/zero -O /dev/zero "http://192.168.0.22:5665/json.htm?type=command&param=setsecstatus&secstatus=2&seccode='..uservariables['SecPanel_pincode']..'" &');
+		os.execute('/home/osmc/tools/alarme.sh init '..uservariables['SecPanel_pincode']..'" &');
 	else if (globalvariables['Security'] == 'Armed Away') then
 		print('(Security) Passage de Armé à Désarmé.')
-		os.execute('wget -o /dev/zero -O /dev/zero "http://192.168.0.22:5665/json.htm?type=command&param=setsecstatus&secstatus=0&seccode='..uservariables['SecPanel_pincode']..'" &');
+		os.execute('/home/osmc/tools/alarme.sh finish '..uservariables['SecPanel_pincode']..'" &');
 	end
 	end
 end
