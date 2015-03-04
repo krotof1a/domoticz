5a0caaffef8949ff8c0f7e14d56496e30229bb8e
krotof1a <christophe.safra@laposte.net>
2015-03-04 19:02:24 +0100

    Correct typo

--- scripts/lua/script_device_security.lua
+++ scripts/lua/script_device_security.lua
@@ -3,10 +3,10 @@ commandArray = {}
 if (devicechanged['Switch Niveau Sécurité'] == 'On') then
 	if (globalvariables['Security'] == 'Disarmed') then
 		print('(Security) Passage de Désarmé à Armé.')
-		os.execute('/home/osmc/tools/alarme.sh init '..uservariables['SecPanel_pincode']..'" &');
+		os.execute('/home/osmc/tools/alarme.sh init '..uservariables['SecPanel_pincode']..' &');
 	else if (globalvariables['Security'] == 'Armed Away') then
 		print('(Security) Passage de Armé à Désarmé.')
-		os.execute('/home/osmc/tools/alarme.sh finish '..uservariables['SecPanel_pincode']..'" &');
+		os.execute('/home/osmc/tools/alarme.sh finish '..uservariables['SecPanel_pincode']..' &');
 	end
 	end
 end
