aa9a879bab71f3ed6e321b63bbfc2d54371434a1
krotof1a <christophe.safra@gmail.com>
2015-03-04 09:24:33 +0100

    Not applicable anymore (on RPI2)

--- scripts/lua/script_time_watchdog.lua
+++
@@ -1,8 +0,0 @@
-commandArray = {}
-
-if (tonumber(otherdevices_svalues['Pi CPU']) > 95) then
-        os.execute ('/home/osmc/tools/killRadioReception.sh')
-	print ('(Watchdog) Killed radioReception process  ...');
-end
-
-return commandArray
