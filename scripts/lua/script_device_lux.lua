91fd51339c00cc6bcc8b97911793cd13731cb8a7
krotof1a <christophe.safra@laposte.net>
2015-03-05 14:49:48 +0100

    Tests

--- scripts/lua/script_device_lux.lua
+++ scripts/lua/script_device_lux.lua
@@ -2,23 +2,23 @@ commandArray = {}
 
 -- Handle Corridor light actions and triggers
 if (devicechanged['Lumière Couloir'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 3 pulse 50')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 3 pulse 50 &')
 	print('(Inter) Couloir actionne.')
 end
 
 -- Handle Parking light actions and triggers
 if (devicechanged['Lumière Garage'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 pulse 50')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 pulse 50 &')
 	print('(Inter) Garage actionne.')
 end
 
 -- Handle Leaving room light actions and triggers
 if (devicechanged['Lumière Salon'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 on')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 on &')
 	print('(Inter) Salon allume.')
 end
 if (devicechanged['Lumière Salon'] == 'Off') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 off')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 off &')
 	print('(Inter) Salon eteint.')
 end
 
