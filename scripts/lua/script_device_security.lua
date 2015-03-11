469431b74655e5ac9c44f0462f6ab6a95b82fbf7
krotof1a <christophe.safra@gmail.com>
2015-03-11 11:41:29 +0100

    Change security switch name and way of working

--- scripts/lua/script_device_security.lua
+++ scripts/lua/script_device_security.lua
@@ -1,14 +1,12 @@
 commandArray = {}
 
-if (devicechanged['Switch Niveau Sécurité'] == 'On') then
-	if (globalvariables['Security'] == 'Disarmed') then
-		print('(Security) Passage de Désarmé à Armé.')
-		os.execute('/home/osmc/tools/alarme.sh init '..uservariables['SecPanel_pincode']..' &');
-	else if (globalvariables['Security'] == 'Armed Away') then
-		print('(Security) Passage de Armé à Désarmé.')
-		os.execute('/home/osmc/tools/alarme.sh finish '..uservariables['SecPanel_pincode']..' &');
-	end
-	end
+if (devicechanged['Sécurité'] == 'On') then
+	print('(Security) Passage de Désarmé à Armé.')
+	os.execute('/home/osmc/tools/alarme.sh init '..uservariables['SecPanel_pincode']..' &');
+else if (devicechanged['Sécurité'] == 'On') then
+	print('(Security) Passage de Armé à Désarmé.')
+	os.execute('/home/osmc/tools/alarme.sh finish '..uservariables['SecPanel_pincode']..' &');
+end
 end
 
 -- Handle alarm
