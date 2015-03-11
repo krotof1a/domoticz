4336a812eec74453b585693110b27dfaa1578491
krotof1a <christophe.safra@gmail.com>
2015-03-11 11:34:56 +0100

    Start subscript asynchronously

--- scripts/lua/script_device_lux.lua
+++ scripts/lua/script_device_lux.lua
@@ -3,19 +3,19 @@ commandArray = {}
 -- Handle Corridor light actions and triggers
 if (devicechanged['Lumière Couloir'] == 'On') then
 	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 3 pulse 50 &')
-	print('(Inter) Couloir actionne.')
+	print('(Inter) Couloir actionné.')
 end
 
 -- Handle Parking light actions and triggers
 if (devicechanged['Lumière Garage'] == 'On') then
 	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 2 pulse 50 &')
-	print('(Inter) Garage actionne.')
+	print('(Inter) Garage actionné.')
 end
 
 -- Handle Leaving room light actions and triggers
 if (devicechanged['Lumière Salon'] == 'On') then
 	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 on &')
-	print('(Inter) Salon allume.')
+	print('(Inter) Salon allumé.')
 end
 if (devicechanged['Lumière Salon'] == 'Off') then
 	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a1 off &')
