cde48fd0cf523055ded78bb7d9846a25dd230c12
krotof1a <christophe.safra@gmail.com>
2015-03-11 11:33:55 +0100

    Start subscript asynchronously

--- scripts/lua/script_device_chaudiere.lua
+++ scripts/lua/script_device_chaudiere.lua
@@ -1,18 +1,18 @@
 commandArray = {}
 if (devicechanged['Etat Chaudiere'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 1 on')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 1 on &')
 	print('(Inter) Chaudiere allumee.')
 end
 if (devicechanged['Etat Chaudiere'] == 'Off') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 1 off')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' 1 off &')
 	print('(Inter) Chaudiere eteinte.')
 end
 if (devicechanged['Chauffage Abrit-piscine'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 on')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 on &')
 	print('(Inter) Chauffage abrit-piscine allume.')
 end
 if (devicechanged['Chauffage Abrit-piscine'] == 'Off') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 off')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' a2 off &')
 	print('(Inter) Chauffage abrit-piscine eteint.')
 end
 return commandArray
