7ab1cce9e676a8486ebe672c323e4ca4a11d4a57
krotof1a <christophe.safra@gmail.com>
2015-03-11 11:35:43 +0100

    Start subscript asynchronously

--- scripts/lua/script_device_portail.lua
+++ scripts/lua/script_device_portail.lua
@@ -1,10 +1,10 @@
 commandArray = {}
 if (devicechanged['Portail'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' portal '..uservariables['Portail_code'])
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' portal '..uservariables['Portail_code']..' &')
 	print('(Inter) Portail actionne.')
 end
 if (devicechanged['Portillon'] == 'On') then
-	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' portal '..uservariables['Portail_code']..' 5000')
+	os.execute ('/home/osmc/tools/sendCS.sh '..uservariables['Emetteur_pin']..' '..uservariables['Emetteur_code']..' portal '..uservariables['Portail_code']..' 5000 &')
 	print('(Inter) Portillon actionne.')
 end
 return commandArray
