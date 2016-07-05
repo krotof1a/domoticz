f1befe415e3a321b0ce0db46fbaf3d1f777ea1e0
krotof1a <christophe.safra@laposte.net>
2016-07-05 10:41:27 +0200

    Ajout fonction de test

---
+++ scripts/lua/script_device_sanityCheck.lua
@@ -0,0 +1,68 @@
+--Script script_time_sanityCheck.lua
+--Vérification post départ au travail
+
+commandArray = {}
+------------------------------------------------------------------------------------
+-- Definition des constantes
+
+sanitySwitchName='Vérification'
+lux001SwitchName='Eclairage Bureau'
+lux002SwitchName='Eclairage Salle-à-manger'
+lux003SwitchName='Eclairage Chambre Parents'
+lux004SwitchName='Eclairage Chambre Amis'
+luxStatus='OK'
+
+ouv001SwitchName='Porte Entrée'
+ouv002SwitchName='Fenêtre Cuisine'
+ouvStatus='OK'
+
+------------------------------------------------------------------------------------
+-- Main program
+
+if (devicechanged[sanitySwitchName] == 'On') then
+
+	-- Vérif lumières
+	print('(SanityCheck) Vérification des lumières')
+	if (otherdevices[lux001SwitchName] == 'On') then
+		print('(SanityCheck) La lumière du bureau est allumée')
+		luxStatus='KO'
+	end
+	if (otherdevices[lux002SwitchName] == 'On') then
+		print('(SanityCheck) La lumière de la salle-à-manger est allumée')		
+		luxStatus='KO'
+	end
+	if (otherdevices[lux003SwitchName] == 'On') then
+		print('(SanityCheck) La lumière de la chambre parentale est allumée')		
+		luxStatus='KO'
+	end
+	if (otherdevices[lux004SwitchName] == 'On') then
+		print('(SanityCheck) La lumière de la chambre d amis est allumée')		
+		luxStatus='KO'
+	end
+
+	-- Vérif Ouvrants
+	print('(SanityCheck) Vérification des porte(s)/fenêtre(s)')
+	if (otherdevices[ouv001SwitchName] == 'Open') then
+		print('(SanityCheck) La porte d entrée est ouverte')
+		ouvStatus='KO'
+	end
+	if (otherdevices[ouv002SwitchName] == 'Open') then
+		print('(SanityCheck) La fenêtre de la cuisine est ouverte')		
+		ouvStatus='KO'
+	end
+	
+	-- Statut vérif
+	if (luxStatus == 'OK') then
+		print('(SanityCheck) Lumières OK')
+	else
+		commandArray['SendNotification']='Lumière(s) allumée(s) à la maison.'
+	end
+	if (ouvStatus == 'OK') then
+		print('(SanityCheck) Ouvrants OK')
+	else
+		commandArray['SendNotification']=commandArray['SendNotification']..'Porte(s)/Fenêtre ouverte(s) à la maison.'
+	end
+	if (luxStatus == 'KO' or ouvStatus == 'KO') then
+		return commandArray
+	end
+end
