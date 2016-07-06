146cfd418d5d98295cfa7bc8da7ac3e6c5fe1e19
krotof1a <christophe.safra@laposte.net>
2016-07-06 22:22:59 +0200

    Avoid using UV as unstable

--- scripts/lua/script_device_chaufPiscine.lua
+++ scripts/lua/script_device_chaufPiscine.lua
@@ -8,8 +8,8 @@ commandArray = {}
 chaufSwitchName='Chauffage Piscine'
 tempJ = tonumber(otherdevices_temperature['Temp Jardin'] or 0)
 tempC = tonumber(otherdevices_svalues['Thermostat Air Chauffage Piscine'] or 0)
-uvCap = tonumber(string.match(otherdevices_svalues['UV'],'([^;]+)') or 0)
-uvCon = tonumber(otherdevices_svalues['Thermostat UV Chauffage Piscine'] or 0)
+--uvCap = tonumber(string.match(otherdevices_svalues['UV'],'([^;]+)') or 0)
+--uvCon = tonumber(otherdevices_svalues['Thermostat UV Chauffage Piscine'] or 0)
 
 ------------------------------------------------------------------------------------
 -- Main program
@@ -23,12 +23,12 @@ if (devicechanged[chaufSwitchName] == 'On') then
 		return commandArray 	
 	end
 	
-	if (uvCap < uvCon) then
-		print('(ChaufPiscine) UV trop bas. Chauffage pisine annulé dans 5s')
-		commandArray[chaufSwitchName]='Off'
-		os.execute ("sleep 5")
-		return commandArray 	
-	end
+	--if (uvCap < uvCon) then
+	--	print('(ChaufPiscine) UV trop bas. Chauffage pisine annulé dans 5s')
+	--	commandArray[chaufSwitchName]='Off'
+	--	os.execute ("sleep 5")
+	--	return commandArray 	
+	--end
 
 end
 
