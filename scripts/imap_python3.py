27761e44a600256aff88f1b447871b8f9e753ad5
krotof1a <christophe.safra@laposte.net>
2016-09-28 09:31:57 +0200

    Rework

--- scripts/imap_python3.py
+++ scripts/imap_python3.py
@@ -9,6 +9,8 @@ import email.header
 import urllib;
 import time;
 import os;
+import urllib2 
+import json
 
 SERVER= sys.argv[1];
 EMAIL_ACCOUNT = sys.argv[2];
@@ -17,6 +19,7 @@ DOMO_HOST = "192.168.1.17"
 DOMO_PORT = "5665"
 interval=10;
 
+# Test that script is not already running
 pidfile = sys.argv[0] + '_' + sys.argv[1] + '.pid'
 if os.path.isfile( pidfile ):
     print "Pid file exists"
@@ -29,10 +32,62 @@ if os.path.isfile( pidfile ):
 else:
     open(pidfile, 'w').close()
 
+# Usefull functions
+def domoticzStatus (switchid):
+  url = "http://"+DOMO_HOST+":"+DOMO_PORT+"/json.htm?type=devices&filter=all&used=true&order=Name"
+  request = urllib2.Request(url)
+  response = urllib2.urlopen(request)
+  json_object = json.loads(response.read())
+  status = 0
+  switchfound = False
+  if json_object["status"] == "OK":
+    for i, v in enumerate(json_object["result"]):
+      if json_object["result"][i]["idx"] == switchid:
+        switchfound = True
+        if json_object["result"][i]["Status"] == "On": 
+          status = 1
+        if json_object["result"][i]["Status"] == "Off": 
+          status = 0
+  if switchfound == False: print "Error. Could not find switch idx in Domoticz response. Defaulting to switch off."
+  return status
+
 def domoticzTalkMessage(msgFrom, msgTo, msgText):
-    msgFull = msgTo+". J'ai un message de " + msgFrom + ". Il dit: " + msgText;
+    if ("Shyrka" in msgFrom):
+	msgFull = msgTo+". "+msgText;
+    else:
+        msgFull = msgTo+". J'ai un message de " + msgFrom + ". Il dit: " + msgText;
     msgSend = urllib.quote(msgFull);
     urllib.urlopen("http://"+DOMO_HOST+":"+DOMO_PORT+"/json.htm?type=command&param=udevice&idx=179&nvalue=0&svalue="+msgSend);
+
+def handleMessage(msgFrom, msgSubj, msgText):
+	if ("christophe." in msgFrom):
+		orig="Christophe"
+	elif ("cecile." in msgFrom):
+		orig="Cécile"
+	elif ("Shyrka@" in msgFrom):
+		orig="Shyrka"
+	else :
+		orig=msgFrom
+
+	if (msgText.startswith("Dis à")) :
+		# Relay message
+		dest=msgText.split(" ")[2];
+		# Check dest is at home
+		if (("Christophe" in dest and domoticzStatus("392")) or
+                    ("Cécile" in dest and domoticzStatus("393"))) :
+			domoticzTalkMessage(orig, dest, " ".join(msgText.split(" ")[4:]))
+    			return 1
+		else :
+			return 0
+	else :
+		if (domoticzStatus("392")):
+			domoticzTalkMessage(orig, "Christophe", msgSubj+msgText)
+			return 1
+		elif (domoticzStatus("393")):
+			domoticzTalkMessage(orig, "Cécile", msgSubj+msgText)
+			return 1
+		else :
+			return 0
 
 def process_mailbox(M):
     """
@@ -58,17 +113,12 @@ def process_mailbox(M):
 	if msg.is_multipart():
     		for part in msg.walk():
 			if ("text/plain" in part.get_content_type()):
-				msg_text = part.get_payload();
+				msg_text = part.get_payload(decode=True);
 	else:
-		msg_text=msg.get_payload();
-        print 'Message %s: %s' % (num, msg_text);
-	if ("christophe.safra" in msg_from):
-		domoticzTalkMessage("Christophe", "Cécile", msg_text);
-	elif ("cecile.houel" in msg_from):
-		domoticzTalkMessage("Cécile", "Christophe", msg_text);
-
-	M.store(num, '+FLAGS', '\\Deleted');
-
+		msg_text=msg.get_payload(decode=True);
+        print 'Message %s: %s : %s' % (num, msg_subj, msg_text);
+	if (handleMessage(msg_from, msg_subj, msg_text)==1):
+		M.store(num, '+FLAGS', '\\Deleted');
 
 M = imaplib.IMAP4(SERVER);
 try:
