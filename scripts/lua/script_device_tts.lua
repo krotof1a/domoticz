6016b62bd9e34e79d69d5f4b21ba806d61948f57
krotof1a <krotof1a>
2016-11-18 21:44:41 +0100

    adapt to CHIP

--- scripts/lua/script_device_tts.lua
+++ scripts/lua/script_device_tts.lua
@@ -5,7 +5,7 @@ if (devicechanged['TTS']) then
 	speach = otherdevices_svalues['TTS']
 	print('(Parle) '..speach)
 	os.execute('pico2wave -l fr-FR -w /tmp/talk.wav "Pause. '..speach..'"')
-	os.execute('aplay -D bluetooth /tmp/talk.wav')
+	os.execute('aplay /tmp/talk.wav')
 	os.execute('rm -f /tmp/talk.wav')
 end
 
