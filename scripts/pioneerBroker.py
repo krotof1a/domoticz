0df79694791dac8918fa527663987db6125907cf
krotof1a <christophe.safra@gmail.com>
2017-10-24 11:38:03 +0200

    Add action handling

--- scripts/pioneerBroker.py
+++ scripts/pioneerBroker.py
@@ -15,13 +15,31 @@ PIONEER_PORT = 8102
 interval=10
 
 def processPioneer():
+  global tn
   try:
   	tn = telnetlib.Telnet(PIONEER_HOST, PIONEER_PORT)
+	print 'Connected to Pioneer AVR'
+	# Process info from Pioneer here ... TBC
+	tn.close()
   except Exception, e:
 	tn.close()
         time.sleep(interval)
         t2=thread.start_new_thread(processPioneer, ())
-
+	
+def processHttpAction( action ):
+  if (action == 'on'):
+	tn.write('PO\r\n')
+  elif (action == 'off'):
+	tn.write('PF\r\n')
+  elif (action[:3] == 'vol'):
+	goal = int(action[3:]) * 1.5
+	if goal < 150
+		tn.write('{:03}'.format(goal) + 'VL\r\n')
+	else:
+		print('Given volume is out of range')
+  else:
+	print('Command not recognized')
+	
 def runHttpServer():
   ADDR = (HTTPD_HOST, HTTPD_PORT)
   serversock = socket(AF_INET, SOCK_STREAM)
@@ -41,14 +59,14 @@ def runHttpServer():
 	        	clientsock.close()
 			string = bytes.decode(data)
 			action = string.split(' ')[1].split('=')[1]
-			print action
+			processHttpAction(action)
   except Exception, e:
 	time.sleep(interval);
 	t1=thread.start_new_thread(runHttpServer, ())
 
 if __name__=='__main__':
+	t2=thread.start_new_thread(processPioneer, ())
 	t1=thread.start_new_thread(runHttpServer, ())
-        t2=thread.start_new_thread(processPioneer, ())
 	# Cause main prog to wait for threads
 	while True:
 		time.sleep(interval)
